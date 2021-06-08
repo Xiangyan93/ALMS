ffcda822fdd609afa3c568438d57a09f4e25c689
Xiangyan93 <hnxxxy123@sjtu.edu.com>
2021-06-09 01:58:25 +0800

    update.

---
+++ scripts_dev/set_mols_unfail.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+CWD = os.path.dirname(os.path.abspath(__file__))
+import sys
+sys.path.append(os.path.join(CWD, '..'))
+from tqdm import tqdm
+from aims.database.models import *
+
+
+def main():
+    mols = session.query(Molecule)
+    for mol in tqdm(mols, total=mols.count()):
+        mol.fail = False
+    session.commit()
+
+
+if __name__ == '__main__':
+    main()
+
