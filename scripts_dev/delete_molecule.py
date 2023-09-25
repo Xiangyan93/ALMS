3188a2b371bf54051581c85b15839a698bcbac27
xiangyan93 <yan.xiang@duke.edu>
2023-09-25 17:30:41 -0400

    update delete_molecule.py

--- scripts_dev/delete_molecule.py
+++ scripts_dev/delete_molecule.py
@@ -5,19 +5,30 @@ from alms.database.models import *
 
 
 class Args(Tap):
-    name: str
+    name: str = None
     """The name of the molecule."""
+    id_range: Tuple[int, int] = None
+    """The range of molecule ids to delete."""
 
 
 def main(args: Args):
     job_manager = Slurm()
     job_manager.update_stored_jobs()
-    mol = session.query(Molecule).filter_by(name=args.name).first()
-    for task in session.query(DoubleMoleculeTask):
-        if task.molecule_id_1 == mol.id or task.molecule_id_2 == mol.id:
-            task.delete(job_manager=job_manager)
-    session.delete(mol)
-    session.commit()
+    mols = session.query(Molecule)
+    delete_mols = []
+    if args.name is not None:
+        delete_mols.append(mols.filter_by(name=args.name).first())
+    if args.id_range is not None:
+        for mol in mols:
+            if args.id_range[0] <= mol.id <= args.id_range[1]:
+                delete_mols.append(mol)
+    for mol in delete_mols:
+        mol.single_molecule_task.delete(job_manager=job_manager)
+        for task in session.query(DoubleMoleculeTask):
+            if task.molecule_id_1 == mol.id or task.molecule_id_2 == mol.id:
+                task.delete(job_manager=job_manager)
+        session.delete(mol)
+        session.commit()
 
 
 if __name__ == '__main__':
